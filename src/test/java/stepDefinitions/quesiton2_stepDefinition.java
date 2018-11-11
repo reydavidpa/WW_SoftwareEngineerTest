4626967760b5a08136820336ae2a530c6854cebc
Rey David Palacios Lazcano <reydavid_x@hotmail.com>
2018-11-10 22:56:54 -0500

    remove unecessay imports

--- src/test/java/stepDefinitions/quesiton2_stepDefinition.java
+++ src/test/java/stepDefinitions/quesiton2_stepDefinition.java
@@ -1,11 +1,9 @@
 package stepDefinitions;
 
-import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import helpers.meetings;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.homepageObjects;
 import pageObjects.meetingSearchObjects;
 import pageObjects.meetingSearchResultsObjects;
